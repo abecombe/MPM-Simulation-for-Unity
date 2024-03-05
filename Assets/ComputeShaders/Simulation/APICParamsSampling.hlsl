10490fe2f7e40a5a14ccc9043ed89afe3f2ba3c8
abecombe <abecombe240@yahoo.co.jp>
2024-03-05 13:14:08 +0900

    change scripts

--- Assets/ComputeShaders/Simulation/APICParamsSampling.hlsl
+++ Assets/ComputeShaders/Simulation/APICParamsSampling.hlsl
@@ -11,16 +11,16 @@
 #define S1(S) (0.75f - S * S)
 #define S2(S) (0.5f * (0.5f + S) * (0.5f + S))
 
-#define INTERPOLATE_X(SUM, C_INDEX, P_INDEX, S, WEIGHT, GRID_VELOCITY_BUFFER) {\
+#define INTERPOLATE_X(SUM, G_POS, C_INDEX, S, WEIGHT, GRID_VELOCITY_BUFFER) {\
 const int3 c_index_0 = C_INDEX + int3(-1, 0, 0);\
 const int3 c_index_1 = C_INDEX + int3(0, 0, 0);\
 const int3 c_index_2 = C_INDEX + int3(1, 0, 0);\
 const uint c_id_0 = CellIndexToCellID(c_index_0);\
 const uint c_id_1 = CellIndexToCellID(c_index_1);\
 const uint c_id_2 = CellIndexToCellID(c_index_2);\
-const float1x4 c_pvec_0 = (float1x4)((c_index_0 - P_INDEX - S) * _GridSpacing, 1.0f);\
-const float1x4 c_pvec_1 = (float1x4)((c_index_1 - P_INDEX - S) * _GridSpacing, 1.0f);\
-const float1x4 c_pvec_2 = (float1x4)((c_index_2 - P_INDEX - S) * _GridSpacing, 1.0f);\
+const float1x4 c_pvec_0 = (float1x4)(CellIndexToWorldPos(c_index_0) - G_POS, 1.0f);\
+const float1x4 c_pvec_1 = (float1x4)(CellIndexToWorldPos(c_index_1) - G_POS, 1.0f);\
+const float1x4 c_pvec_2 = (float1x4)(CellIndexToWorldPos(c_index_2) - G_POS, 1.0f);\
 float3x4 sum_x = 0;\
 sum_x += mul((float3x1)GRID_VELOCITY_BUFFER[c_id_0], c_pvec_0) * S0(S.x);\
 sum_x += mul((float3x1)GRID_VELOCITY_BUFFER[c_id_1], c_pvec_1) * S1(S.x);\
@@ -29,30 +29,30 @@ sum_x *= WEIGHT;\
 SUM += sum_x;\
 }\
 
-#define INTERPOLATE_Y(SUM, C_INDEX, P_INDEX, S, WEIGHT, GRID_VELOCITY_BUFFER) {\
+#define INTERPOLATE_Y(SUM, G_POS, C_INDEX, S, WEIGHT, GRID_VELOCITY_BUFFER) {\
 float3x4 sum_y = 0;\
-INTERPOLATE_X(sum_y, C_INDEX + int3(0, -1, 0), P_INDEX, S, S0(S.y), GRID_VELOCITY_BUFFER)\
-INTERPOLATE_X(sum_y, C_INDEX + int3(0, 0, 0), P_INDEX, S, S1(S.y), GRID_VELOCITY_BUFFER)\
-INTERPOLATE_X(sum_y, C_INDEX + int3(0, 1, 0), P_INDEX, S, S2(S.y), GRID_VELOCITY_BUFFER)\
+INTERPOLATE_X(sum_y, G_POS, C_INDEX + int3(0, -1, 0), S, S0(S.y), GRID_VELOCITY_BUFFER)\
+INTERPOLATE_X(sum_y, G_POS, C_INDEX + int3(0, 0, 0), S, S1(S.y), GRID_VELOCITY_BUFFER)\
+INTERPOLATE_X(sum_y, G_POS, C_INDEX + int3(0, 1, 0), S, S2(S.y), GRID_VELOCITY_BUFFER)\
 sum_y *= WEIGHT;\
 SUM += sum_y;\
 }\
 
-#define INTERPOLATE_Z(SUM, C_INDEX, P_INDEX, S, GRID_VELOCITY_BUFFER) {\
-INTERPOLATE_Y(SUM, C_INDEX + int3(0, 0, -1), P_INDEX, S, S0(S.z), GRID_VELOCITY_BUFFER)\
-INTERPOLATE_Y(SUM, C_INDEX + int3(0, 0, 0), P_INDEX, S, S1(S.z), GRID_VELOCITY_BUFFER)\
-INTERPOLATE_Y(SUM, C_INDEX + int3(0, 0, 1), P_INDEX, S, S2(S.z), GRID_VELOCITY_BUFFER)\
+#define INTERPOLATE_Z(SUM, G_POS, C_INDEX, S, GRID_VELOCITY_BUFFER) {\
+INTERPOLATE_Y(SUM, G_POS, C_INDEX + int3(0, 0, -1), S, S0(S.z), GRID_VELOCITY_BUFFER)\
+INTERPOLATE_Y(SUM, G_POS, C_INDEX + int3(0, 0, 0), S, S1(S.z), GRID_VELOCITY_BUFFER)\
+INTERPOLATE_Y(SUM, G_POS, C_INDEX + int3(0, 0, 1), S, S2(S.z), GRID_VELOCITY_BUFFER)\
 }\
 
-#define INTERPOLATE(B_VELOCITY, C_INDEX, S, GRID_VELOCITY_BUFFER) \
-INTERPOLATE_Z(B_VELOCITY, C_INDEX, C_INDEX, S, GRID_VELOCITY_BUFFER)\
+#define INTERPOLATE(B_VELOCITY, G_POS, C_INDEX, S, GRID_VELOCITY_BUFFER) \
+INTERPOLATE_Z(B_VELOCITY, G_POS, C_INDEX, S, GRID_VELOCITY_BUFFER)\
 
 #define SAMPLE_APIC_PARAMS(PARTICLE, GRID_VELOCITY_BUFFER) \
 float3x4 b_velocity = 0;\
 const float3 g_pos = WorldPosToGridPos(PARTICLE.position);\
 const int3 c_index = round(g_pos - 0.5f);\
 const float3 s = (g_pos - 0.5f) - round(g_pos - 0.5f);\
-INTERPOLATE(b_velocity, c_index, s, GRID_VELOCITY_BUFFER)\
+INTERPOLATE(b_velocity, g_pos, c_index, s, GRID_VELOCITY_BUFFER)\
 float3x3 B;\
 B._m00_m01_m02 = b_velocity._m00_m01_m02;\
 B._m10_m11_m12 = b_velocity._m10_m11_m12;\
